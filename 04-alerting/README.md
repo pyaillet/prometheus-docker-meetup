09e766e8a131236c524c34b61920023e53057269
Pierre-Yves Aillet <pyaillet@gmail.com>
2018-02-21 09:18:20 +0100

    📖 Doc

--- 04-alerting/README.md
+++ 04-alerting/README.md
@@ -2,6 +2,7 @@
 
  - Launch with `docker-compose up -d` or `./launch.sh`
  - Go to http://localhost:8081
- - Go to http://localhost:8081/source.html
- - Go to http://localhost:8081/metrics
+ - Go to http://localhost:9090/rules
+ - Go to http://localhost:9090/alerts
+ - Go to http://localhost:9093/#/alerts
  - Go to http://localhost:3000/dashboard/db/alien-dashboard?orgId=1
--- 04-alerting/launch.sh
+++ 04-alerting/launch.sh
@@ -4,7 +4,9 @@ docker-compose up --build -d
 
 if [ $(uname) == "Darwin" ]; then
     open http://localhost:8081
-    open http://localhost:8081/metrics
+    open http://localhost:9090/rules
+    open http://localhost:9090/alerts
+    open http://localhost:9093/#/alerts
     sleep 3
     open "http://localhost:3000/dashboard/db/alien-dashboard?orgId=1&refresh=1s&from=now-5m&to=now"
 fi
